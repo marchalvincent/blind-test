c6f48ef4b62530b92e9bf4a9f738b8cf00108a40
Takfa <Takfa@Takfa-Board>
2012-01-09 15:27:55 +0100

    managers ok + new bd

--- blind-test/blind_test.sql
+++ blind-test/blind_test.sql
@@ -1,33 +1,92 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Serveur: localhost
+-- Généré le : Lun 09 Janvier 2012 à 14:18
+-- Version du serveur: 5.1.53
+-- Version de PHP: 5.3.4
 
-DROP SCHEMA IF EXISTS `blind_test` ;
-CREATE SCHEMA IF NOT EXISTS `blind_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `blind_test` ;
-
--- -----------------------------------------------------
--- Table `blind_test`.`banque`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `blind_test`.`banque` ;
-
-CREATE TABLE IF NOT EXISTS `blind_test`.`banque` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `answer` VARCHAR(60) NOT NULL ,
-  `name` CHAR(37) NOT NULL ,
-  `directory` VARCHAR(100) NOT NULL ,
-  `version` INT NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-CREATE INDEX `index_banque_answer` ON `blind_test`.`banque` (`answer` ASC) ;
-
-CREATE INDEX `index_banque_name` ON `blind_test`.`banque` (`name` ASC) ;
-CREATE INDEX `index_banque_directory` ON `blind_test`.`banque` (`directory` ASC) ;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Base de données: `blind_test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `banque`
+--
+
+CREATE TABLE IF NOT EXISTS `banque` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `answer` varchar(60) NOT NULL,
+  `name` char(37) NOT NULL,
+  `directory` varchar(100) NOT NULL,
+  `version` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_banque_answer` (`answer`),
+  KEY `index_banque_name` (`name`),
+  KEY `index_banque_directory` (`directory`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Contenu de la table `banque`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stat`
+--
+
+CREATE TABLE IF NOT EXISTS `stat` (
+  `idStat` int(11) NOT NULL AUTO_INCREMENT,
+  `defaite` int(11) NOT NULL,
+  `victoire` int(11) NOT NULL,
+  PRIMARY KEY (`idStat`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Contenu de la table `stat`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `idStat` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `login` varchar(60) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  PRIMARY KEY (`idUser`),
+  UNIQUE KEY `idStat` (`idStat`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Contenu de la table `user`
+--
+
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`idStat`) REFERENCES `stat` (`idStat`) ON DELETE CASCADE ON UPDATE CASCADE;
