1f891bf3c7a7e74cc10cbd6c112b9eb0fe10c627
francois <francois@localhost>
2012-01-09 14:41:11 +0100

    regles.txt

---
+++ blind-test/src/org/client/ui/AbstractPanel.java
@@ -0,0 +1,41 @@
+package org.client.ui;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JPanel;
+
+/**
+ * Classe de base pour les panels avec :
+ * layout
+ * contraintes
+ * marges
+ * @author francois
+ *
+ */
+public abstract class AbstractPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private GridBagLayout layout = new GridBagLayout();
+	private GridBagConstraints contraintes = new GridBagConstraints();
+	private Insets marges = new Insets(0, 0, 0, 0);
+	
+	protected abstract void initPanel ();
+	
+	@Override
+	public GridBagLayout getLayout () {
+		return layout;
+	}
+	
+	protected GridBagConstraints getContraintes () {
+		return contraintes;
+	}
+	
+	protected Insets getMarges () {
+		return marges;
+	}
+}
---
+++ blind-test/test/org/server/persistence/PersistenceSuite.java
@@ -0,0 +1,11 @@
+package org.server.persistence;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BanqueManagerTest.class })
+public class PersistenceSuite {
+
+}
