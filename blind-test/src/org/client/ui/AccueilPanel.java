0e045c422de894078dc7a606c15d34710f0dcd9d
Vincent <Vincent@Vincent-PC.formation.local>
2012-01-09 15:16:52 +0100

    maj IHM

--- blind-test/src/org/client/ui/AccueilPanel.java
+++ blind-test/src/org/client/ui/AccueilPanel.java
@@ -3,6 +3,7 @@ package org.client.ui;
 import java.awt.GridBagConstraints;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 import org.client.ui.listeners.DeconnexionListener;
 import org.client.ui.listeners.JouerListener;
@@ -25,36 +26,58 @@ public class AccueilPanel extends AbstractPanel {
 	@Override
 	protected void initPanel () {
 		setLayout(getLayout());
-		
-		//Bouton Deconnexion
-		JButton boutonDeconnexion = new JButton ("Déconnexion");
-		boutonDeconnexion.addMouseListener(new DeconnexionListener ());
-		getContraintes().gridx = 0;
-		getContraintes().gridy = 1;
-		getContraintes().gridwidth = GridBagConstraints.REMAINDER;
-		getMarges().set(0, 15, 15, 15);
-		getContraintes().insets = getMarges();
-		//getContraintes().anchor = GridBagConstraints.LINE_END;
-		this.add(boutonDeconnexion, getContraintes());
-		
-		//Bouton Jouer
-		JButton boutonJouer = new JButton ("Jouer !");
-		boutonJouer.addMouseListener(new JouerListener ());;
-		getContraintes().gridy = 2;
-		//getContraintes().anchor = GridBagConstraints.REMAINDER;
-		this.add(boutonJouer, getContraintes());
-		
-		//Bouton Règles
-		JButton boutonRegles = new JButton ("Règles");
-		boutonRegles.addMouseListener(new ReglesListener ());
-		getContraintes().gridy = 3;
-		this.add(boutonRegles, getContraintes());
-		
+				
 		//Bouton Quitter
 		JButton boutonQuitter = new JButton ("Quitter");
 		boutonQuitter.addMouseListener(new QuitterListener ());
 		getContraintes().gridx = 0;
 		getContraintes().gridy = 0;
+		getContraintes().weighty = 0.6;
+		getContraintes().weightx = 0.5;
+		getContraintes().gridwidth = GridBagConstraints.RELATIVE;
+		getContraintes().anchor = GridBagConstraints.FIRST_LINE_START;
+		getContraintes().insets = getMarges();
+		getMarges().set(30, 15, 0, 0);
 		this.add(boutonQuitter, getContraintes());
+		
+		//Bouton Deconnexion
+		JButton boutonDeconnexion = new JButton ("Déconnexion");
+		boutonDeconnexion.addMouseListener(new DeconnexionListener ());
+		getContraintes().gridx = 1;
+		getContraintes().gridy = 0;
+		getContraintes().weighty = 0.6;
+		getContraintes().weightx = 0.5;
+		getContraintes().gridwidth = GridBagConstraints.REMAINDER;
+		getContraintes().anchor = GridBagConstraints.FIRST_LINE_END;
+		getMarges().set(30, 0, 0, 15);
+		this.add(boutonDeconnexion, getContraintes());
+		
+		//Bouton Jouer
+		JButton boutonJouer = new JButton ("Jouer !");
+		boutonJouer.addMouseListener(new JouerListener ());
+		getContraintes().gridx = 0;
+		getContraintes().gridy = 1;
+		getContraintes().weighty = 0.1;
+		getContraintes().weightx = GridBagConstraints.REMAINDER;
+		getContraintes().anchor = GridBagConstraints.PAGE_END;
+		this.add(boutonJouer, getContraintes());
+		
+		//Bouton Règles
+		JButton boutonRegles = new JButton ("Règles");
+		boutonRegles.addMouseListener(new ReglesListener ());
+		getContraintes().gridx = 0;
+		getContraintes().gridy = 2;
+		getContraintes().weighty = 0.1;
+		getContraintes().weightx = GridBagConstraints.REMAINDER;
+		getContraintes().anchor = GridBagConstraints.PAGE_START;
+		this.add(boutonRegles, getContraintes());
+
+		//Un espace pour ne pas que les composants ne soient tout en bas
+		JLabel espace = new JLabel (" ");
+		getContraintes().gridx = 0;
+		getContraintes().gridy = 3;
+		getContraintes().weighty = 0.4;
+		getContraintes().gridwidth = GridBagConstraints.REMAINDER;
+		this.add(espace, getContraintes());
 	}
 }
