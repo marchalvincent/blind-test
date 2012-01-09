a78003dd710c1c815ef23196a910a2f24873e9a2
francois <francois@localhost>
2012-01-09 17:50:07 +0100

    Fusionner

--- blind-test/src/org/server/main/ServerMain.java
+++ blind-test/src/org/server/main/ServerMain.java
@@ -1,5 +1,6 @@
 package org.server.main;
 
+import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Level;
 
@@ -10,6 +11,7 @@ import org.commons.exception.BlindTestException;
 import org.commons.logger.InfoProvider;
 import org.commons.logger.InfoProviderManager;
 import org.commons.util.SystemUtil;
+import org.server.concurrent.Server;
 import org.server.monitor.MonitorRunnable;
 import org.server.persistence.EnumDatabaseProperties;
 import org.server.persistence.Managers;
@@ -36,6 +38,14 @@ public final class ServerMain {
 		final String locHostname = locProperties.getProperty(EnumDatabaseProperties.HOSTNAME.getConstName());
 		final String locPort = locProperties.getProperty(EnumDatabaseProperties.PORT.getConstName());
 		locProvider.appendMessage(Level.INFO, String.format("La connexion à la base de données a été effectuée. L'adresse de la base est \"%s\" sur le port \"%s\".", locHostname, locPort));
+		final Server locServer = new Server(locProvider);
+		try {
+			locServer.start();
+		} catch (IOException e) {
+			locProvider.appendMessage(Level.SEVERE, String.format("Impossible de lancer le serveur sur le port %s", locPort), e);
+			locProvider.appendMessage(Level.SEVERE, "Fermeture de l'application");
+			SystemUtil.exit();
+		}
 	}
 	
 	static private final Configuration loadConfiguration(final String[] parArguments) {
