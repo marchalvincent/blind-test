daca39e915af5e03561928c032c936dfda6a9452
o.pitton <o.pitton@localhost>
2012-01-09 12:54:02 +0100

    fix

---
+++ blind-test/src/org/commons/message/AbstractMessage.java
@@ -0,0 +1,23 @@
+package org.commons.message;
+
+public abstract class AbstractMessage implements IMessage {
+
+	private static final long serialVersionUID = 1L;
+	private final Integer id;
+	protected AbstractMessage (EnumMessage parMessage) {
+		
+		id = parMessage.getId ();
+		
+	}
+	
+	@Override
+	public Integer getId () {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return id.toString();
+	}
+
+}
---
+++ blind-test/src/org/commons/message/IMessage.java
@@ -0,0 +1,10 @@
+package org.commons.message;
+
+import java.io.Serializable;
+
+import org.commons.util.IWithId;
+
+public interface IMessage extends IWithId, Serializable {
+	
+
+}
