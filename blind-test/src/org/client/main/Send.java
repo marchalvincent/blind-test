7bd41b39449632e07594b7fa5b32ed8c8833217d
drakeng <drakeng@localhost>
2012-01-09 12:31:46 +0100

    Ajout de la partie Client

--- blind-test/src/org/client/main/Send.java
+++ blind-test/src/org/client/main/Send.java
@@ -1,4 +1,4 @@
-package org.server.main;
+package org.client.main;
 
 import java.io.PrintWriter;
 import java.util.Scanner;
@@ -6,20 +6,21 @@ import java.util.Scanner;
 public class Send implements Runnable {
 
 	private PrintWriter out;
-	private String message = null;
+	private String login = null, message = null;
 	private Scanner sc = null;
 	
 	public Send (PrintWriter out) {
 		this.out = out;
+		
 	}
 
 	
 	public void run() {
 		
-		  sc = new Scanner(message);
+		  sc = new Scanner(System.in);
 		  
 		  while(true){
-			    System.out.println(message);
+			    System.out.println(login + " : ");
 				message = sc.nextLine();
 				out.println(message);
 			    out.flush();
