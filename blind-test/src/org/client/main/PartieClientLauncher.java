9d1206d4a94e3b750a1182d4aea249bf693a9dbd
francois <francois@localhost>
2012-01-10 20:02:11 +0100

    bouton annuler panel inscription

---
+++ blind-test/src/org/client/main/PartieClientLauncher.java
@@ -0,0 +1,49 @@
+package org.client.main;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+
+import org.client.ui.JouerPanel;
+import org.commons.configuration.Configuration;
+import org.commons.configuration.ConfigurationManager;
+import org.commons.logger.InfoProvider;
+import org.commons.logger.InfoProviderManager;
+import org.commons.util.SystemUtil;
+
+public class PartieClientLauncher {
+
+	private JouerPanel fenetre = null;
+	private String login = null;
+	
+	public PartieClientLauncher(JouerPanel fenetre, String login) {
+		this.fenetre = fenetre;
+		this.login = login;
+	}
+	
+	public void startPartieClient() {
+		Configuration config = ConfigurationManager.getConfiguration();
+		InfoProvider fileProvider = InfoProviderManager.getUiInfoProvider();
+		
+		Socket socket = null;
+		try {
+			
+			socket = new Socket(config.getHostName(), config.getPort());
+			
+			ThreadPartieEcriture tEcriture = new ThreadPartieEcriture(socket, fenetre, login);
+			ThreadPartieLecture tLecture = new ThreadPartieLecture(socket, tEcriture);
+			
+			Thread threadEcriture = new Thread (tEcriture);
+			Thread threadLecture = new Thread (tLecture);
+			
+			threadEcriture.start();
+			threadLecture.start();
+			
+		} catch (IOException e) {
+			fileProvider.appendMessage(Level.SEVERE, "Inscription - erreur de connexion au serveur");
+			SystemUtil.close(socket);
+		}
+		
+		SystemUtil.close(socket);
+	}
+}
---
+++ blind-test/src/org/client/main/ThreadPartieLecture.java
@@ -0,0 +1,64 @@
+package org.client.main;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+
+import org.commons.logger.InfoProvider;
+import org.commons.logger.InfoProviderManager;
+import org.commons.message.DisplayMessage;
+import org.commons.message.EnumMessage;
+import org.commons.message.IMessage;
+import org.commons.message.InfoMessage;
+import org.commons.message.WinnerMessage;
+import org.commons.util.WithUtilities;
+import org.server.concurrent.ReadWriterUtil;
+
+public class ThreadPartieLecture implements Runnable {
+
+	private Socket socket = null;
+	private ThreadPartieEcriture tEcriture = null;
+	
+	public ThreadPartieLecture(Socket socket, ThreadPartieEcriture tEcriture) {
+		super();
+		this.socket = socket;
+		this.tEcriture = tEcriture;
+	}
+
+	@Override
+	public void run() {
+		
+		InfoProvider fileProvider = InfoProviderManager.getUiInfoProvider();
+		
+		while (true) {
+			try {
+				IMessage messageRetour = ReadWriterUtil.read(socket);
+				EnumMessage mess = WithUtilities.getById(EnumMessage.values(), messageRetour.getId());
+				
+				if (EnumMessage.isDisplay(mess)) {
+					tEcriture.addIMessage(messageRetour);
+				}
+				else if (EnumMessage.isError(mess)) {
+					DisplayMessage display = (DisplayMessage) EnumMessage.DISPLAY.createMessage();
+					display.setFileName(tEcriture.getCurrentImage());
+					tEcriture.addIMessage(display);
+				}
+				else if (EnumMessage.isWinner(mess)) {
+					InfoMessage info = (InfoMessage) EnumMessage.INFO.createMessage();
+					info.setMessage("ACK - WINNER");
+					ReadWriterUtil.write(socket, info);
+					
+					WinnerMessage win = (WinnerMessage) messageRetour;
+					fileProvider.appendMessage(Level.INFO, "Bravo à l'utilisateur " + win.getLogin() + " qui a trouvé la bonne réponse !");
+				}
+				
+				
+			} catch (ClassNotFoundException e) {}
+			catch (IOException e) {
+				fileProvider.appendMessage(Level.SEVERE, "Inscription - erreur de connexion au serveur");
+			}
+		}
+	}
+	
+	
+}
