9c499d1e02961ef06a669b47db6e147ddb97102d
francois <francois@localhost>
2012-01-11 17:26:15 +0100

    fix ui

--- blind-test/src/org/client/ui/parties/PartieTask.java
+++ blind-test/src/org/client/ui/parties/PartieTask.java
@@ -4,21 +4,34 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.swing.JFrame;
+
+import org.client.ui.PartiesPanel;
 import org.commons.cache.Caches;
 
 public class PartieTask extends TimerTask {
 
 	Timer timer;
 	List <String> listParties;
+	private JFrame _partiesWindow;
+	private PartiesPanel _partiesPanel;
 	
 	public PartieTask (Timer timer) {
 		this.timer = timer;
+		listParties = Caches.parties().keys();
+		_partiesWindow = new JFrame ("Parties");
+		_partiesPanel = new PartiesPanel(listParties).initPanel();
+		_partiesWindow.setContentPane(_partiesPanel);
+		_partiesWindow.setSize(300, 300);
+		_partiesWindow.setVisible(true);
 	}
 	
 	
 	@Override
 	public void run() {
 		listParties = Caches.parties().keys();
+		_partiesPanel.refresh(listParties);
+		_partiesWindow.validate();
 	    timer.cancel(); 
 	    }
 
