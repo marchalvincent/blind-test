f140a5e038f545bf86e097b14fefdc47407e34f0
o.pitton <o.pitton@localhost>
2012-01-12 11:38:17 +0100

    fix bug usermanager

--- blind-test/src/org/client/ui/parties/PartieTask.java
+++ blind-test/src/org/client/ui/parties/PartieTask.java
@@ -58,7 +58,7 @@ public class PartieTask extends TimerTask {
 			}
 			ListGamesMessage locGameMessage = (ListGamesMessage) locMessageRetour;
 			List<String> listGames = locGameMessage.getListGames();
-			
+			System.out.println("List Partie " + listGames);
 			_partiesPanel.refresh(listGames);
 			_partiesWindow.validate();
 
