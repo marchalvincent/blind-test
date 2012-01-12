0c1627c357f4847f14ad6e7fe77063f263f9d647
Vincent <Vincent@Vincent-PC.formation.local>
2012-01-12 11:37:36 +0100

    up stat takfa

--- blind-test/src/org/client/ui/listeners/StatListener.java
+++ blind-test/src/org/client/ui/listeners/StatListener.java
@@ -26,12 +26,12 @@ import org.server.concurrent.ReadWriterUtil;
  *
  */
 public class StatListener extends AbstractBoutonListener {
-	
+
 	private final String _login;
 	private  AbstractPanel _panel;
 	public StatListener(final String parLogin, AbstractPanel panel, BoutonGris bouton) {
 		super(bouton);
-		
+
 		_login = parLogin;
 		_panel = panel;
 	}
@@ -39,25 +39,35 @@ public class StatListener extends AbstractBoutonListener {
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		super.mouseClicked(e);
-		
+
 		Configuration config = ConfigurationManager.getConfiguration();
 		InfoProvider fileProvider = InfoProviderManager.getUiInfoProvider();
 		String displayMessage ="";
 		StatMessage locStatMessage = (StatMessage) EnumMessage.STAT.createMessage();
 		locStatMessage.setLogin(_login);
 		Socket socket = null;
-		
+
 		try {
 			socket = new Socket(config.getHostName(), config.getPort());
 			ReadWriterUtil.write(socket,locStatMessage);
 			//on écoute la réponse
 			IMessage messageRetour = null;
-				messageRetour = ReadWriterUtil.read(socket);
-			if (messageRetour instanceof StatMessage) {
-				StatMessage msgStatRetour = (StatMessage) messageRetour;
-				displayMessage = "Nombre de victoires : "+msgStatRetour.getVictoire()+"\n"+"Nombre de défaites : "+msgStatRetour.getDefaite();	
+			messageRetour = ReadWriterUtil.read(socket);
+			
+			if (messageRetour instanceof StatMessage == false) {
+				fileProvider.appendMessage(Level.SEVERE, "Le type du message est incorrect. Le message reçu est : " + messageRetour);
+				SystemUtil.close(socket);
+				return;
 			}
-				
+			
+			StatMessage msgStatRetour = (StatMessage) messageRetour;
+			int locPercent = msgStatRetour.getVictoire()+msgStatRetour.getDefaite();
+			if(locPercent == 0) locPercent= 1;
+			
+			int percentVictoire = (int)(msgStatRetour.getVictoire()/locPercent) * 100;
+			int percentDefaite = (int)(msgStatRetour.getDefaite()/locPercent) * 100;
+			displayMessage = "Nombre de victoires : "+msgStatRetour.getVictoire()+"\n"+"Nombre de défaites : "+msgStatRetour.getDefaite()+"\n"+"% Victoires : "+percentVictoire+"%"+"\n"+"% Défaites : "+percentDefaite+"%";
+			
 		} catch (IOException e1) {
 			fileProvider.appendMessage(Level.SEVERE, String.format("Impossible d'écrire dans la socket d'adresse %s", socket.getInetAddress().getHostAddress()), e1);
 		}
