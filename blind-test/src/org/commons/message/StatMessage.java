3a75e5e3f64d731e17145efb57d62b762b6da589
o.pitton <o.pitton@localhost>
2012-01-12 16:29:09 +0100

    taille partie + wrong answer

---
+++ blind-test/src/org/commons/message/StatMessage.java
@@ -0,0 +1,49 @@
+package org.commons.message;
+
+public class StatMessage extends AbstractMessage {
+
+	private static final long serialVersionUID = 1L;
+	private String login;
+	private Integer victoire;
+	private Integer defaite;
+
+	protected StatMessage() {
+		this ("", 0, 0);
+	}
+	
+	protected StatMessage(String login, Integer victoire, Integer defaite) {
+		super(EnumMessage.STAT);
+		this.login = login;
+		this.victoire = victoire;
+		this.defaite = defaite;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public Integer getVictoire() {
+		return victoire;
+	}
+
+	public void setVictoire(Integer victoire) {
+		this.victoire = victoire;
+	}
+
+	public Integer getDefaite() {
+		return defaite;
+	}
+
+	public void setDefaite(Integer defaite) {
+		this.defaite = defaite;
+	}
+	
+	public int getScore(){
+		return victoire.intValue() - defaite.intValue();
+	}
+
+}
